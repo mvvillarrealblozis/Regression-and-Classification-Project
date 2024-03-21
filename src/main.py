9e7ce2895511ddc65db147b39db8861454bcda4f
mvvillarrealblozis <mvvillarrealblozis@dons.usfca.edu>
2024-03-21 13:25:57 -0700

    Renaming vars and setting up regression analysis

--- src/main.py
+++ src/main.py
@@ -28,12 +28,12 @@ clothingData.fillna({'Division Name': 'Unknown', 'Department Name': 'Unknown', '
 missing_values_after = clothingData.isnull().sum()
 
 # For classification, we're interested in Age, Rating, Department Name, Class Name, and Recommended IND
-features_classification = ['Age', 'Rating', 'Department Name', 'Class Name']
-target_classification = 'Recommended IND'
+featuresClassification = ['Age', 'Rating', 'Department Name', 'Class Name']
+targetClassification = 'Recommended IND'
 
 # Convert categorical features to dummy variables
-df_classification = pd.get_dummies(clothingData[features_classification], drop_first=True)
-df_classification['Recommended IND'] = clothingData[target_classification]
+dfClassification = pd.get_dummies(clothingData[featuresClassification], drop_first=True)
+dfClassification['Recommended IND'] = clothingData[targetClassification]
 
 #print(missing_values_after, df_classification.head())
 
@@ -44,8 +44,8 @@ from sklearn.metrics import classification_report, accuracy_score
 from sklearn.preprocessing import StandardScaler
 
 # Separate features and target variable for classification
-X_classification = df_classification.drop('Recommended IND', axis=1)
-y_classification = df_classification['Recommended IND']
+X_classification = dfClassification.drop('Recommended IND', axis=1)
+y_classification = dfClassification['Recommended IND']
 
 # Splitting the dataset into training and testing sets
 X_train_class, X_test_class, y_train_class, y_test_class = train_test_split(X_classification, y_classification, test_size=0.3, random_state=42)
@@ -56,13 +56,19 @@ X_train_class_scaled = scaler.fit_transform(X_train_class)
 X_test_class_scaled = scaler.transform(X_test_class)
 
 # Initialize and train logistic regression model
-log_reg = LogisticRegression(max_iter=1000)
-log_reg.fit(X_train_class_scaled, y_train_class)
+logisticRegression = LogisticRegression(max_iter=1000)
+logisticRegression.fit(X_train_class_scaled, y_train_class)
 
 # Predictions and evaluation
-predictions_class = log_reg.predict(X_test_class_scaled)
-accuracy = accuracy_score(y_test_class, predictions_class)
-classification_rep = classification_report(y_test_class, predictions_class)
+predictionsClass = logisticRegression.predict(X_test_class_scaled)
+accuracy = accuracy_score(y_test_class, predictionsClass)
+classification_rep = classification_report(y_test_class, predictionsClass)
 
 print(accuracy, classification_rep)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+target_regression = 'Positive Feedback Count'
 
